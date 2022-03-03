0daf300efbca338f22eaf1b2ffcc91565b043307
Harshil Chudasama <74813791+iamharshil@users.noreply.github.com>
2022-03-03 13:27:58 +0530

    Update read.php

--- read.php
+++ read.php
@@ -1,27 +1,24 @@
 <?php
-include './db.php';
-$sql = "select * from demo_table";
-$result = $conn->query($sql);
-while($row = $result->fetch_assoc()) {
-	echo "<tr>";
-	if ($row['id'] == $_GET['id']) {
-		echo '<form class=""form-inline m-2" action="update.php" method="POST">';
-		echo '<td><input type="text" class="form-control" name="name" value="'.$row['name'].'"></td>';
-		echo '<td><input type="number" class="form-control" name="score" value="'.$row['score'].'"></td>';
-		echo '<td><button type="submit" class="btn btn-primary">Save</button></td>';
-		echo '<input type="hidden" name="id" value="' .$row['id'].'">';
-		echo '</form>';
-	}
-	else{
+	include './db.php';
+	$sql = "select * from demo_table";
+	$result = $conn->query($sql);
+	while($row = $result->fetch_assoc()) { 
+		if(isset($_GET['id']) && $row['id'] == $_GET['id']) { // if ($row['id'] == $_GET['id'])
+			echo '<form class="form-inline m-2" action="update.php" method="POST">';
+			echo '<td><input type="text" class="form-control" name="name" value="'.$row['name'].'"></td>';
+			echo '<td><input type="number" class="form-control" name="score" value="'.$row['score'].'"></td>';
+			echo '<td><button type="submit" class="btn btn-primary">Save</button></td>';
+			echo '<input type="hidden" name="id" value="' .$row['id'].'">';
+			echo '</form>';
+		} else {
 
-	echo "<tr>";
-	echo "<td>";
-	echo "<td>" . $row['name'] . "</td>";
-	echo "<td>" . $row['score'] . "</td>";
-	echo '<td> <a class="btn btn-primary" href="index.php>id=' . $row['id'] . '" role="button">Update</a>';
-	echo '<td><a class="btn btn-primary" href="index.php?id=' .$row['id'] . '" role="button">Update</a></td>';
-	echo '<td><a class="btn btn-danger" href="delete.php?id=' . $row['id'] . '" role="button">Delete</a></td>';
-	echo "</tr>";
-}}
-$conn->close();
+		echo "<tr>";
+		echo "<td>" . $row['name'] . "</td>";
+		echo "<td>" . $row['score'] . "</td>";
+		echo '<td><a class="btn btn-primary" href="index.php?id=' .$row['id'] . '" role="button">Update</a></td>';
+		echo '<td><a class="btn btn-danger" href="delete.php?id=' . $row['id'] . '" role="button">Delete</a></td>';
+		echo "</tr>";
+	}
+}
+	$conn->close();
 ?>
